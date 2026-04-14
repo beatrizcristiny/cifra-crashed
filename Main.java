776625a0170713ad6c1f0cc2cc0c60f286602eec
beatrizcristiny <beatrizcrispires@gmail.com>
2026-04-13 21:22:15 -0300

    fazendo com o neris

--- Main.java
+++ Main.java
@@ -3,16 +3,29 @@ public class Main{
     public static void main (String [] args){
         Scanner scanner = new Scanner (System.in);
         String frase;
-        char acentos[] = ('´', "`", "^", "~", ";", ".", ",", "'", "!", "¨");
+        System.out.println("Digite uma frase de até 47 letras");
+        frase = scanner.nextLine().toUpperCase();
+        String novafrase = "";
+        char [] acentos = {'Ã','Á','À','Â','É','Ê','Í','Ó','Ô','Õ','Ú','Ç'};
+        char [] normais = {'A','A','A','A','E','E','I','O','O','O','U','C'};
 
-        System.out.println("Digite uma frase");
-        frase = scanner.nextLine();
-
-        if (frase :  char acentos){
-            frase : "";
+        for (int i = 0; i < frase.length(); i++){
+             char letra = frase.charAt(i);
+             
+             
+             for (int b = 0; b < acentos.length; b++){
+             if (letra == acentos[b]) {
+                letra = normais[b];
+             }    
+             
         }
-
-        
-
+        if (letra >= 'A' && letra <= 'Z') {
+            novafrase = novafrase + letra;
+        }
+     }
+   
+    System.out.println(novafrase);
     }
+
+   
 }

f1432f14e9dbc0d685f198fe743cc257356d622f
beatrizcristiny <beatrizcrispires@gmail.com>
2026-04-15 08:39:28 -0300

    pegando o codigo que fiz junto com o neris pq o meu estava dando erro

--- Main.java
+++ Main.java
@@ -1,31 +1,39 @@
-import java.util.Scanner;
-public class Main{
-    public static void main (String [] args){
-        Scanner scanner = new Scanner (System.in);
-        String frase;
-        System.out.println("Digite uma frase de até 47 letras");
-        frase = scanner.nextLine().toUpperCase();
-        String novafrase = "";
-        char [] acentos = {'Ã','Á','À','Â','É','Ê','Í','Ó','Ô','Õ','Ú','Ç'};
-        char [] normais = {'A','A','A','A','E','E','I','O','O','O','U','C'};
+    import java.util.Scanner;
+    public class Main {
 
-        for (int i = 0; i < frase.length(); i++){
-             char letra = frase.charAt(i);
-             
-             
-             for (int b = 0; b < acentos.length; b++){
-             if (letra == acentos[b]) {
-                letra = normais[b];
-             }    
-             
-        }
-        if (letra >= 'A' && letra <= 'Z') {
-            novafrase = novafrase + letra;
-        }
-     }
-   
-    System.out.println(novafrase);
-    }
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
-   
+            System.out.println("Digite uma frase:");
+            String frase = scanner.nextLine().toUpperCase();
+
+            if (frase.length() > 47) {
+                System.out.println("Não é possivel cifrar, número maximo de caracteres a mais que o permitido");
+            } else {
+                char[] acentos = {'Ã', 'Â', 'Á', 'Ä', 'Ë', 'É', 'Ê', 'Ö', 'Ô', 'Õ', 'Í', 'Ü', 'Ú', 'Ç'};
+                char[] normais = {'A', 'A', 'A', 'A', 'E', 'E', 'E', 'O', 'O', 'O', 'I', 'U', 'U', 'C'};
+
+                char[] letrasFrase = frase.toCharArray();
+                String novafrase = "";
+
+                for (int i = 0; i < letrasFrase.length; i++) {
+                    char letraAtual = letrasFrase[i];
+
+                    for (int b = 0; b < acentos.length; b++) {
+                        if (letraAtual == acentos[b]) {
+                            letraAtual = normais[b];
+                            break;
+                        }
+                    }
+
+                    if (letraAtual >= 'A' && letraAtual <= 'Z') {
+                        novafrase = novafrase + letraAtual;
+                    }
+                }
+
+                System.out.println("Frase formatada: " + novafrase);
+            }
+
+      scanner.close();
+   } 
 }
